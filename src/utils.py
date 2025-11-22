22ee88433bbad92b6bfd828d944456b205dea487
Debcode2006 <sarkardebanjan137@gmail.com>
2025-11-23 02:06:33 +0530

    added frontend

--- src/utils.py
+++ src/utils.py
@@ -45,7 +45,19 @@ def evaluate_model(x_train, y_train, x_test, y_test, models):
             
         return report
     
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+    
+def load_object(file_path):
+    '''
+    Loads the object.
+    '''
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return pickle.load(file_obj)
     
     
     except Exception as e:
         raise CustomException(e,sys)
+        
